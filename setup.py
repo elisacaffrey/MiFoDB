516e0b6a3beb13ed044bddcf37399bda3bf9ddc4
Elisa Caffrey <16585164+elisacaffrey@users.noreply.github.com>
2024-03-03 16:37:21 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+from MiFoDB._version import __version__
+
+setup(name='MiFoDB',
+      version=__version__,
+      description='Calculation of strain-level metrics',
+      url='https://github.com/elisacaffrey/MiFoDB',
+      author='Elisa Caffrey and Matt Olm',
+      author_email='ecaffrey@stanford.edu',
+      license='MIT',
+      package_data={'inStrain': ['helper_files/NullModel.txt']},
+      #packages=['inStrain'],
+      packages=find_packages(exclude=["tests"]),
+      scripts=['bin/inStrain'],
+      python_requires='>=3.4.0',
+      install_requires=[
+          'numpy',
+          'pandas>=0.25,!=1.1.3',
+          'seaborn',
+          'matplotlib',
+          'biopython<=1.74',
+          'tqdm',
+          'pysam>=0.15', # This sets a requirement for python 3.7 for now, but so be it. pysam v0.9 (which works on python 3.8) has a broken iterator (has no stop)
+          'networkx',
+          'h5py',
+          'psutil',
+          'lmfit',
+          'pytest'
+      ],
+      zip_safe=False)
