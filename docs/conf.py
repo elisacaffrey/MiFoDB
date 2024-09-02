a2b91f0770849bb89dc438ad6b8b7c630610a10b
Elisa Caffrey <16585164+elisacaffrey@users.noreply.github.com>
2024-09-02 16:32:12 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -61,3 +61,6 @@ html_css_files = [
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = [
+    'extra.css',
+]
