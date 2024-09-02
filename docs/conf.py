db181560a08dd849d61129cf5c5619c75f4c15e7
Elisa Caffrey <16585164+elisacaffrey@users.noreply.github.com>
2024-09-02 16:35:43 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -53,10 +53,6 @@ html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # html_theme = 'alabaster'
 
-html_css_files = [
-    'source/css/custom.css',
-]
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
